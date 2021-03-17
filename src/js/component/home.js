4f999d762b3e3bdba63aa292407f2eeec882e9c7
FedericoBeeche <fbeeche@gmail.com>
2021-03-17 06:21:40 +0000

    trying to make it delete things

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,39 +2,38 @@ import React, { useState } from "react";
 
 //create your first component
 export function ToDo() {
-    const [userInput, setUserInput] = useState("");
-    const [task, setTask] = useState([
-        "Estudiar",
-        "Hacer tarea",
-        "Comer pizza"
-    ]);
+	const [userInput, setUserInput] = useState("");
+	const [task, setTask] = useState([]);
 
-    const handleInfo = () => {
-        if (userInput != null) {
-            let newArray = [...task, userInput];
-            setTask(newArray);
-            setUserInput("");
-        }
-    };
+	const handleInfo = () => {
+		if (userInput != null) {
+			let newArray = [...task, userInput];
+			setTask(newArray);
+			setUserInput("");
+		}
+	};
 
-    const deleteTask = id => {
-        task.splice(id, 1);
-        setTask([...task]);
-    };
+	const deleteTask = id => {
+		task.splice(id, 1);
+		setTask([...task]);
+	};
 
-    return (
-        <>
-            <div className="container"></div>
-            <input
-                type="text"
-                required
-                value={userInput}
-                onChange={e => setUserInput(e.target.value)}
-                onKeyPress={e => (e.key === "Enter" ? handleInfo() : "")}
-            />
-            {task.map((final, id) => (
-                <p key={id}>{final}</p>
-            ))}
-        </>
-    );
+	return (
+		<div>
+			<div className="container"></div>
+			<input
+				type="text"
+				required
+				value={userInput}
+				onChange={e => setUserInput(e.target.value)}
+				onKeyPress={e => (e.key === "Enter" ? handleInfo() : "")}
+			/>
+			{task.map((final, id) => (
+				<p onClick={deleteTask} key={id}>
+					{final}
+					<button onClick={() => deleteTask()}>Delete</button>
+				</p>
+			))}
+		</div>
+	);
 }
