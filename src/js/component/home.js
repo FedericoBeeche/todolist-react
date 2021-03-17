1fd9ce3f55b72a6e26b140688771ff941d323690
FedericoBeeche <fbeeche@gmail.com>
2021-03-17 02:58:40 +0000

    actualizacion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,18 +2,39 @@ import React, { useState } from "react";
 
 //create your first component
 export function ToDo() {
-	const [task, setTask] = useState("");
+    const [userInput, setUserInput] = useState("");
+    const [task, setTask] = useState([
+        "Estudiar",
+        "Hacer tarea",
+        "Comer pizza"
+    ]);
 
-	return (
-		<>
-			<div className="container"></div>
-			<input
-				type="text"
-				required
-				value={task}
-				onChange={e => setTask(e.target.value)}
-			/>
-			<p>{task}</p>
-		</>
-	);
+    const handleInfo = () => {
+        if (userInput != null) {
+            let newArray = [...task, userInput];
+            setTask(newArray);
+            setUserInput("");
+        }
+    };
+
+    const deleteTask = id => {
+        task.splice(id, 1);
+        setTask([...task]);
+    };
+
+    return (
+        <>
+            <div className="container"></div>
+            <input
+                type="text"
+                required
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+                onKeyPress={e => (e.key === "Enter" ? handleInfo() : "")}
+            />
+            {task.map((final, id) => (
+                <p key={id}>{final}</p>
+            ))}
+        </>
+    );
 }
